aa7278b99a24450b3c36a013e83bf93829dae133
MrJStyle <438747096lmz@gmail.com>
2022-07-28 23:08:08 +0800

    feature(fastapi_with_cyac): init commit

---
+++ web/fastapi/main.py
@@ -0,0 +1,12 @@
+import json
+from mmap import mmap
+from multiprocessing.shared_memory import SharedMemory
+
+from cyac import AC
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+
+
---
+++ web/fastapi/tests/test_fastapi_with_cyac.py
@@ -0,0 +1,5 @@
+from fastapi_with_cyac import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
